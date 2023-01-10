db75ea128c6c27e3205166dbfe0e2d745f64f1e1
GameSevenbjj <rueben.beard@gmail.com>
2023-01-10 18:09:36 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+// Animations
+AOS.init({
+  anchorPlacement: "top-left",
+  duration: 1000,
+});
+//For the loading screen
+setTimeout(() => {
+  document.querySelector(".loading_wrapper").style.display = "none";
+}, 3000);
+
+const imgQuote = document.querySelector(".img-overly");
+const btn = document.querySelector();
+
+function on() {
+  document.getElementsByClassName("img-overly").style.display = "block";
+}
